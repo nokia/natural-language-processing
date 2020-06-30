3c11c51c09e9e48e292aa4b22f563b12230e404c
edepanaf <depanafieuelie@gmail.com>
2020-06-30 16:15:01 +0200

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# © 2020 Nokia
+# Licensed under the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+# !/usr/bin/env python3
+# coding: utf-8
+# Author: Élie de Panafieu  <elie.de_panafieu@nokia-bell-labs.com>
